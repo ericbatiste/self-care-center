6eff545ac06f736318b830de9da233c9a6df9e47
Eric Batiste <ericbatiste4@gmail.com>
2023-10-27 15:54:57 -0600

    Add ability to navigate to View All Msgs page.

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 var affirmBtn = document.querySelector("#affirmation");
 var mantraBtn = document.querySelector("#mantra");
 var receiveMsgBtn = document.querySelector("#receive-msg-btn");
+var viewAllBtn = document.querySelector("#view-all-btn");
+var goHomeBtn = document.querySelector('.go-home-btn');
 var msgContainer = document.querySelector(".msg-container");
+var inputsContainer = document.querySelector('.inputs-container');
+var viewAllMsgs = document.querySelector('.view-all-msgs');
 
 var currentMessage;
 
-receiveMsgBtn.addEventListener("click", receiveMessage);
+receiveMsgBtn.addEventListener('click', receiveMessage);
+viewAllBtn.addEventListener('click', showViewAllPage);
+goHomeBtn.addEventListener('click', showHomePage)
 
 function receiveMessage() {
   if (!affirmBtn.checked && !mantraBtn.checked) return;
@@ -22,6 +28,29 @@ function receiveMessage() {
 
 function renderMessage() {
     msgContainer.innerText = currentMessage;
+}
+
+function showViewAllPage() {
+    toggle(msgContainer);
+    toggle(inputsContainer);
+    toggle(viewAllBtn);
+    toggle(viewAllMsgs);
+}
+
+function showHomePage() {
+    toggle(msgContainer);
+    toggle(inputsContainer);
+    toggle(viewAllBtn);
+    toggle(viewAllMsgs);    
+}
+
+function toggle(element) {
+    if (element.classList.contains('hidden')) {
+        element.classList.remove('hidden');
+    } else {
+        element.classList.add('hidden');
+    }
+    return element;
 }
 
 function getRandomMessage(array) {
