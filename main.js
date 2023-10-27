50db3cc10c551749e41a07d5c7ce11407386611d
Eric Batiste <ericbatiste4@gmail.com>
2023-10-27 09:04:19 -0600

    Implement radio button and render msg functionality when user clicks receive msg btn.

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
+var affirmBtn = document.querySelector("#affirmation");
+var mantraBtn = document.querySelector("#mantra");
+var receiveMsgBtn = document.querySelector("#receive-msg-btn");
+var msgContainer = document.querySelector(".msg-container");
 
+var currentMessage;
+
+receiveMsgBtn.addEventListener("click", receiveMessage);
+
+function receiveMessage() {
+  if (!affirmBtn.checked && !mantraBtn.checked) return;
+  if (affirmBtn.checked) {
+    affirmBtn.checked = false;
+    currentMessage = affirmations[getRandomMessage(affirmations)];
+  }
+  if (mantraBtn.checked) {
+    mantraBtn.checked = false;
+    currentMessage = mantras[getRandomMessage(mantras)];
+  }
+  renderMessage()
+}
+
+function renderMessage() {
+    msgContainer.innerText = currentMessage;
+}
+
+function getRandomMessage(array) {
+  return Math.floor(Math.random() * array.length);
+}
