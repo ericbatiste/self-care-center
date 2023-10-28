8848f141fb9579b2e92456a3c9eff6ae4cb828d5
Eric Batiste <ericbatiste4@gmail.com>
2023-10-28 10:51:18 -0500

    Implement function to render messages from data.js arrays to their respective lists on view all messages page. Add event listenter to window load to call the function.

--- main.js
+++ main.js
@@ -6,9 +6,12 @@ var goHomeBtn = document.querySelector('.go-home-btn');
 var msgContainer = document.querySelector(".msg-container");
 var inputsContainer = document.querySelector('.inputs-container');
 var viewAllMsgs = document.querySelector('.view-all-msgs');
+var affirmList = document.querySelector('.affirm-list');
+var mantraList = document.querySelector('.mantra-list');
 
 var currentMessage;
 
+window.addEventListener('load', renderAllMessageLists)
 receiveMsgBtn.addEventListener('click', receiveMessage);
 viewAllBtn.addEventListener('click', showViewAllPage);
 goHomeBtn.addEventListener('click', showHomePage);
@@ -28,6 +31,15 @@ function receiveMessage() {
 
 function renderMessage() {
     msgContainer.innerText = currentMessage;
+}
+
+function renderAllMessageLists() {
+    for (var i = 0; i < affirmations.length; i++) {
+        affirmList.innerHTML += `<li>${affirmations[i].text}</li>`
+    }
+    for (var i = 0; i < mantras.length; i++) {
+        mantraList.innerHTML += `<li>${mantras[i].text}</li>`
+    }
 }
 
 function showViewAllPage() {
