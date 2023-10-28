a1ea2727e3efd681998b9f2107aa82b4cdcdd3e8
Eric Batiste <ericbatiste4@gmail.com>
2023-10-28 13:33:59 -0500

    Implement user ability to add new messages to each list.

--- main.js
+++ main.js
@@ -2,19 +2,25 @@ var affirmBtn = document.querySelector("#affirmation");
 var mantraBtn = document.querySelector("#mantra");
 var receiveMsgBtn = document.querySelector("#receive-msg-btn");
 var viewAllBtn = document.querySelector("#view-all-btn");
-var goHomeBtn = document.querySelector('.go-home-btn');
+var goHomeBtn = document.querySelector(".go-home-btn");
 var msgContainer = document.querySelector(".msg-container");
-var inputsContainer = document.querySelector('.inputs-container');
-var viewAllMsgs = document.querySelector('.view-all-msgs');
-var affirmList = document.querySelector('.affirm-list');
-var mantraList = document.querySelector('.mantra-list');
+var inputsContainer = document.querySelector(".inputs-container");
+var viewAllMsgs = document.querySelector(".view-all-msgs");
+var affirmList = document.querySelector(".affirm-list");
+var mantraList = document.querySelector(".mantra-list");
+var newAffirmForm = document.querySelector("#new-affirm-form");
+var newMantraForm = document.querySelector("#new-mantra-form");
+var newAffirmText = document.querySelector("#new-affirmation");
+var newMantraText = document.querySelector("#new-mantra");
 
 var currentMessage;
 
-window.addEventListener('load', renderAllMessageLists)
-receiveMsgBtn.addEventListener('click', receiveMessage);
-viewAllBtn.addEventListener('click', showViewAllPage);
-goHomeBtn.addEventListener('click', showHomePage);
+window.addEventListener("load", renderAllMessageLists);
+receiveMsgBtn.addEventListener("click", receiveMessage);
+viewAllBtn.addEventListener("click", showViewAllPage);
+goHomeBtn.addEventListener("click", showHomePage);
+newAffirmForm.addEventListener("submit", addNewAffirmation);
+newMantraForm.addEventListener("submit", addNewMantra);
 
 function receiveMessage() {
   if (!affirmBtn.checked && !mantraBtn.checked) return;
@@ -26,45 +32,82 @@ function receiveMessage() {
     mantraBtn.checked = false;
     currentMessage = mantras[getRandomMessage(mantras)].text;
   }
-  renderMessage()
+  renderMessage();
 }
 
 function renderMessage() {
-    msgContainer.innerText = currentMessage;
+  msgContainer.innerText = currentMessage;
 }
 
 function renderAllMessageLists() {
-    for (var i = 0; i < affirmations.length; i++) {
-        affirmList.innerHTML += `<li>${affirmations[i].text}</li>`
-    }
-    for (var i = 0; i < mantras.length; i++) {
-        mantraList.innerHTML += `<li>${mantras[i].text}</li>`
-    }
+  affirmList.innerHTML = "";
+  for (var i = 0; i < affirmations.length; i++) {
+    affirmList.innerHTML += `
+        <div class="list-msg">
+            <li>${affirmations[i].text}</li>
+            <button class="delet-btn">-</button>
+        </div>`;
+  }
+  mantraList.innerHTML = "";
+  for (var i = 0; i < mantras.length; i++) {
+    mantraList.innerHTML += `
+        <div class="list-msg">
+            <li>${mantras[i].text}</li>
+            <button class="delet-btn">-</button>
+        </div>`;
+  }
+}
+
+// function addNewMessage(message, array) {
+//     event.preventDefault();
+//     if (!message.value) return;
+//     array.push(createNewMessage(message.value))
+//     renderAllMessageLists()
+// }
+
+function addNewAffirmation() {
+  event.preventDefault();
+  if (!newAffirmText.value) return;
+  affirmations.push(createNewMessage(newAffirmText.value));
+  renderAllMessageLists();
+  newAffirmForm.reset();
+}
+
+function addNewMantra() {
+  event.preventDefault();
+  if (!newMantraText.value) return;
+  mantras.push(createNewMessage(newMantraText.value));
+  renderAllMessageLists();
+  newMantraForm.reset();
+}
+
+function createNewMessage(text) {
+  return { id: Date.now(), text: text };
 }
 
 function showViewAllPage() {
-    toggle(msgContainer);
-    toggle(inputsContainer);
-    toggle(viewAllBtn);
-    toggle(viewAllMsgs);
-    toggle(goHomeBtn);
+  toggle(msgContainer);
+  toggle(inputsContainer);
+  toggle(viewAllBtn);
+  toggle(viewAllMsgs);
+  toggle(goHomeBtn);
 }
 
 function showHomePage() {
-    toggle(msgContainer);
-    toggle(inputsContainer);
-    toggle(viewAllBtn);
-    toggle(viewAllMsgs);
-    toggle(goHomeBtn);    
+  toggle(msgContainer);
+  toggle(inputsContainer);
+  toggle(viewAllBtn);
+  toggle(viewAllMsgs);
+  toggle(goHomeBtn);
 }
 
 function toggle(element) {
-    if (element.classList.contains('hidden')) {
-        element.classList.remove('hidden');
-    } else {
-        element.classList.add('hidden');
-    }
-    return element;
+  if (element.classList.contains("hidden")) {
+    element.classList.remove("hidden");
+  } else {
+    element.classList.add("hidden");
+  }
+  return element;
 }
 
 function getRandomMessage(array) {
